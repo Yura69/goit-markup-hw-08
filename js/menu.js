dd6f3c306382d151b17f36ecddee8ca648404456
Yura Dziubenko <68082402+Yura69@users.noreply.github.com>
2020-09-14 23:55:27 +0300

    Выполненое дз №8

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,11 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
+    const mobilMenuRef = document.querySelector("[data-menu]");
 
     menuBtnRef.addEventListener("click", () => {
-        menuBtnRef.classList.toggle("is-open");
+        const expanded =
+            menuBtnRef.classList.toggle("is-open");
+        mobilMenuRef.classList.toggle("is-open");
+
     })
 })()
